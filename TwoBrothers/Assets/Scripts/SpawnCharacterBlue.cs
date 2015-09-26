68fd4e7ad3ace848b0663712b27f036324445fef
Ross Grambo <rgrambo@uw.edu>
2015-09-26 13:08:55 -0700

    Two Spawns

---
+++ TwoBrothers/Assets/Scripts/SpawnCharacterBlue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCharacterBlue : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		if (Network.isClient) {
+			StartCoroutine("DelayedStart");
+		}
+	}
+	
+	// Update is called once per frame
+	IEnumerator DelayedStart() {
+		yield return new WaitForSeconds(2);
+		Network.Instantiate(player, this.transform.position, this.transform.rotation, 0);
+	}
+}
--- TwoBrothers/Assets/Scripts/SpawnCharacterRed.cs
+++ TwoBrothers/Assets/Scripts/SpawnCharacterRed.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawnCharacter : MonoBehaviour {
+public class SpawnCharacterRed : MonoBehaviour {
 
 	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine("DelayedStart");
+		if (Network.isServer) {
+			StartCoroutine("DelayedStart");
+		}
 	}
 	
 	// Update is called once per frame
 	IEnumerator DelayedStart() {
-		yield return new WaitForSeconds(5);
+		yield return new WaitForSeconds(2);
 		Network.Instantiate(player, this.transform.position, this.transform.rotation, 0);
 	}
 }
