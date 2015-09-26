f24975146ce7616683bc3ee08eb9604ccb717651
Ross Grambo <rgrambo@uw.edu>
2015-09-26 15:21:38 -0700

    Added death mesh

---
+++ TwoBrothers/Assets/CollideLevelReset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideLevelReset : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<NetworkView>() != null) {
+			if (other.GetComponent<NetworkView>().isMine) {
+				GetComponent<NetworkView>().RPC("ResetLevel", RPCMode.AllBuffered);
+			}
+		}
+	}
+
+	[RPC]
+	void ResetLevel() {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
