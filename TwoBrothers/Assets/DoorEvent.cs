07d15f9777827187aae9430d9279041d42eff8c5
Ross Grambo <rgrambo@uw.edu>
2015-09-26 13:52:36 -0700

    Buttons and doors

---
+++ TwoBrothers/Assets/DoorEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorEvent : MonoBehaviour {
+
+	public void Fire() {
+		this.transform.Translate(Vector3.Slerp(this.transform.position, this.transform.position + Vector3.down * 10, 0.5f));
+	}
+}
---
+++ TwoBrothers/Assets/EventKickoff.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventKickoff : MonoBehaviour {
+
+	bool serverButton = false;
+	bool clientButton = false;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<NetworkView>().isMine) {
+			GetComponent<NetworkView>().RPC("EventHappened", RPCMode.AllBuffered, Network.isServer);
+		}
+	}
+
+	void EventHappened(bool serverSent) {
+		if (!serverSent) {
+			clientButton = true;
+		} else {
+			serverButton = true;
+		}
+
+		if (serverButton && clientButton) {
+			TriggerDoorOpen();
+		}
+	}
+
+	void TriggerDoorOpen() {
+		GameObject.Find("DoorRed").GetComponent<DoorEvent>().Fire();
+		GameObject.Find("DoorBlue").GetComponent<DoorEvent>().Fire();
+	}
+}
