311fcac69ac3848e5871625118f15fa34c1047d6
Ross Grambo <rgrambo@uw.edu>
2015-09-26 14:29:20 -0700

    Another button technique

--- TwoBrothers/Assets/EventKickoff.cs
+++ TwoBrothers/Assets/EventKickoff.cs
@@ -3,32 +3,37 @@ using System.Collections;
 
 public class EventKickoff : MonoBehaviour {
 
-	bool serverButton = false;
-	bool clientButton = false;
+	public bool Clicked;
 
 	void OnTriggerEnter(Collider other) {
 		if (other.GetComponent<NetworkView>() != null) {
 			if (other.GetComponent<NetworkView>().isMine) {
-				GetComponent<NetworkView>().RPC("EventHappened", RPCMode.AllBuffered, Network.isServer);
+				GetComponent<NetworkView>().RPC("EventHappened", RPCMode.AllBuffered, Network.isServer, true);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.GetComponent<NetworkView>() != null) {
+			if (other.GetComponent<NetworkView>().isMine) {
+				GetComponent<NetworkView>().RPC("EventHappened", RPCMode.AllBuffered, Network.isServer, false);
 			}
 		}
 	}
 
 	[RPC]
-	void EventHappened(bool serverSent) {
-		if (!serverSent) {
-			clientButton = true;
-			print("client!!!");
-		} else {
-			serverButton = true;
-			print("SERVERRRR!!!");
-		}
+	void EventHappened(bool serverSent, bool value) {
+		Clicked = value;
 
-		if (serverButton && clientButton) {
-			TriggerDoorOpen();
+		if (Network.isServer) {
+			var buttons = GameObject.FindGameObjectsWithTag("Room1Button");
+			if (buttons[0].GetComponent<EventKickoff>().Clicked && buttons[1].GetComponent<EventKickoff>().Clicked) {
+				GetComponent<NetworkView>().RPC("TriggerDoorOpen", RPCMode.AllBuffered, Network.isServer);
+			}
 		}
 	}
 
+	[RPC]
 	void TriggerDoorOpen() {
 		GameObject.Find("DoorRed").GetComponent<DoorEvent>().Fire();
 		GameObject.Find("DoorBlue").GetComponent<DoorEvent>().Fire();
